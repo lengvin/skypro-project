0b3f8cd8978521f9a0a660f0799a0556b7c4c6cd
lengvin <zve565ro@gmail.com>
2025-07-16 00:15:07 +0700

    refactoring test for masks.py

--- tests/test_masks.py
+++ tests/test_masks.py
@@ -1,10 +1,25 @@
 from src import masks
-from src import widget
+import pytest
 
 
-def test_card_number(card_number):
-    assert masks.get_mask_card_number(7000792289606361) == card_number
+@pytest.mark.parametrize('card_number, result', [
+    (7000792289606361, '7000 79** **** 6361'),
+    (3928457690236251, '3928 45** **** 6251'),
+    ('7000792289606361', '7000 79** **** 6361'),
+    (453457, '4534 57** **** '),
+    ('some_text', 'some _t** **** '),
+    ('', ' ** **** ')
+])
+def test_card_number(card_number, result):
+    assert masks.get_mask_card_number(card_number) == result
 
 
-def test_account_number(account_number):
-    assert masks.get_mask_account(73654108430135874305) == account_number
+@pytest.mark.parametrize('account_number, result', [
+    (73654108430135874305, '**4305'),
+    (12332489832489834924, '**4924'),
+    ('73654108430135874305', '**4305'),
+    ('some_text', '**text'),
+    (23434342, '**4342'),
+])
+def test_account_number(account_number, result):
+    assert masks.get_mask_account(account_number) == result
