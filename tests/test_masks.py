a168df47b4c208070ca23d07d06d883e0b655e3a
lengvin <zve565ro@gmail.com>
2025-07-16 21:29:55 +0700

    add some test data for test_masks.py

--- tests/test_masks.py
+++ tests/test_masks.py
@@ -7,7 +7,7 @@ import pytest
     (3928457690236251, '3928 45** **** 6251'),
     ('7000792289606361', '7000 79** **** 6361'),
     (453457, '4534 57** **** '),
-    ('some_text', 'some _t** **** '),
+    (5432759478037935028938573245, '5432 75** **** 7935'),
     ('', ' ** **** ')
 ])
 def test_card_number(card_number, result):
@@ -18,8 +18,9 @@ def test_card_number(card_number, result):
     (73654108430135874305, '**4305'),
     (12332489832489834924, '**4924'),
     ('73654108430135874305', '**4305'),
-    ('some_text', '**text'),
+    (47563247562385258724568325, '**8325'),
     (23434342, '**4342'),
+    (12, '**12')
 ])
 def test_account_number(account_number, result):
     assert masks.get_mask_account(account_number) == result
