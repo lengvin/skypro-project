4e8f46b522c23ec9b703b37c991a928f6efbcb5d
lengvin <zve565ro@gmail.com>
2025-07-18 04:26:15 +0700

    refactoring tests according to PEP8

--- tests/test_masks.py
+++ tests/test_masks.py
@@ -1,36 +1,37 @@
-from src import masks
+from typing import Union
+
 import pytest
 
+from src import masks
 
-def test_get_mask_card_number(card_numbers):
+
+def test_get_mask_card_number(card_numbers: list) -> None:
     for data in card_numbers:
         card_number, result = data
         assert masks.get_mask_card_number(card_number) == result
 
 
-@pytest.mark.parametrize('invalid_data', [
-    453457,
-    5432759478037935028938573245,
-    0,
-    '',
-])
-def test_invalid_card_number(invalid_data):
+@pytest.mark.parametrize(
+    "invalid_data",
+    [
+        453457,
+        5432759478037935028938573245,
+        0,
+        "",
+    ],
+)
+def test_invalid_card_number(invalid_data: Union[str, int]) -> None:
     with pytest.raises(ValueError):
         masks.get_mask_card_number(invalid_data)
 
 
-def test_get_mask_account(account_numbers):
+def test_get_mask_account(account_numbers: list) -> None:
     for data in account_numbers:
         account_number, result = data
         assert masks.get_mask_account(account_number) == result
 
 
-@pytest.mark.parametrize('invalid_data', [
-    47563247562385258724568325,
-    23434342,
-    0,
-    ''
-])
-def test_invalid_account_number(invalid_data):
+@pytest.mark.parametrize("invalid_data", [47563247562385258724568325, 23434342, 0, ""])
+def test_invalid_account_number(invalid_data: Union[str, int]) -> None:
     with pytest.raises(ValueError):
         masks.get_mask_account(invalid_data)
