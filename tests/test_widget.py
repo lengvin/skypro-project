ef99651beea8d24c74fcbfd2bbadd055e5b46257
lengvin <zve565ro@gmail.com>
2025-07-18 00:42:12 +0700

    refactoring test_widget.py

--- tests/test_widget.py
+++ tests/test_widget.py
@@ -1,10 +1,37 @@
+import pytest
+
 from src import widget
 
 
-def test_mask_account_card(card_data, account_data):
-    assert widget.mask_account_card('Visa Platinum 7000792289606361') == card_data
-    assert widget.mask_account_card('Счет 73654108430135874305') == account_data
+def test_mask_account_card(bank_cards):
+    for card_data in bank_cards:
+        card, result = card_data
+        assert widget.mask_account_card(card) == result
 
 
-def test_get_date(date_data):
-    assert widget.get_date('2024-03-11T02:26:18.671407') == date_data
+@pytest.mark.parametrize('invalid_data', [
+    'Maestro7000792289606361',
+    'Счет45930725495749350875',
+    'VisaPlatinum7000792289606361',
+    'Maestro',
+    7000792289606361
+])
+def test_invalid_account_card(invalid_data):
+    with pytest.raises(ValueError):
+        widget.mask_account_card(invalid_data)
+
+
+def test_get_date(date_examples):
+    for date_data in date_examples:
+        data, result = date_data
+        assert widget.get_date(data) == result
+
+
+@pytest.mark.parametrize('invalid_data', [
+    '2025-06-2402:26:18.671407',
+    'T02:26:18.671407',
+    '2025-6-4T02:26:18.671407'
+])
+def test_invalid_date(invalid_data):
+    with pytest.raises(ValueError):
+        widget.get_date(invalid_data)
