dfeb22ab5cb4cb11ad2567a3b31492341d9923d9
lengvin <zve565ro@gmail.com>
2025-07-21 21:54:13 +0700

    add docstrings for generators.py

--- src/generators.py
+++ src/generators.py
@@ -1,15 +1,24 @@
 def filter_by_currency(list_of_transactions, currency):
+    """
+    Итератор, сортирующий транзакции по валюте
+    """
     for transaction in list_of_transactions:
         if transaction["operationAmount"]["currency"]["name"] == currency:
             yield transaction
 
 
 def transaction_descriptions(list_of_transactions):
+    """
+    Генератор, возвращающий описание транзакций
+    """
     for transaction in list_of_transactions:
         yield transaction["description"]
 
 
 def card_number_generator(start_num, end_num):
+    """
+    Генератор номеров банковских карт с указанным диапазоном
+    """
     for i in range(start_num, end_num + 1):
         str_card_numbers = '0' * (16 - len(str(i))) + str(i)
         yield ' '.join(map(lambda x: str_card_numbers[x * 4: (x + 1) * 4], range(4)))
