9df69c48a7c0f8643c0a9b1604cb63a8b2bedf4e
lengvin <zve565ro@gmail.com>
2025-07-24 18:11:15 +0700

    refactoring module generators.py

--- src/generators.py
+++ src/generators.py
@@ -3,7 +3,7 @@ def filter_by_currency(list_of_transactions, currency):
     Итератор, сортирующий транзакции по валюте
     """
     for transaction in list_of_transactions:
-        if transaction["operationAmount"]["currency"]["name"] == currency:
+        if transaction["operationAmount"]["currency"]["code"] == currency:
             yield transaction
 
 
@@ -18,7 +18,11 @@ def transaction_descriptions(list_of_transactions):
 def card_number_generator(start_num, end_num):
     """
     Генератор номеров банковских карт с указанным диапазоном
+    Максимальный допустимый диапазон от 0 до 9999999999999999
     """
-    for i in range(start_num, end_num + 1):
-        str_card_numbers = '0' * (16 - len(str(i))) + str(i)
-        yield ' '.join(map(lambda x: str_card_numbers[x * 4: (x + 1) * 4], range(4)))
+    if len(str(start_num)) <= 16 and len(str(end_num)) <= 16:
+        for i in range(start_num, end_num + 1):
+            str_card_numbers = '0' * (16 - len(str(i))) + str(i)
+            yield ' '.join(map(lambda x: str_card_numbers[x * 4: (x + 1) * 4], range(4)))
+    else:
+        yield 'Некорректные крайние значения'
