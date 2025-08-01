ee9ac6422a3b55d2d89a9d1036beb88ca5f709b9
lengvin <zve565ro@gmail.com>
2025-08-01 20:35:25 +0700

    refactoring test_utils.py

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,5 +1,6 @@
 from src import utils
 import json
+import pytest
 
 
 def test_read_json_file():
@@ -10,3 +11,12 @@ def test_read_json_file():
         dict_result = json.load(file)
 
     assert func_result == dict_result
+
+
+@pytest.mark.parametrize('invalid_data', [
+    'data/test.json',
+    {},
+    'test'
+])
+def test_errors_read_json_file(invalid_data):
+    assert utils.read_json_file(invalid_data) == []
