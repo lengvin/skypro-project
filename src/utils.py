e2e63e7f401cb1d71b0fbc0134e0256e57e3aa2b
lengvin <zve565ro@gmail.com>
2025-08-04 12:42:34 +0700

    add logging in module utils.py

--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,14 @@
 import json
+import logging
+
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='%(asctime)s - %(filename)s - %(levelname)s - %(message)s',
+    filename='../logs/utils.log',
+    filemode='w'
+)
+module_logger = logging.getLogger(__name__)
 
 
 def read_json_file(file_path):
@@ -7,8 +17,11 @@ def read_json_file(file_path):
     """
     try:
         with open(file_path, encoding='utf-8') as file:
+            module_logger.info('Открытие JSON-файла')
             json_file = json.load(file)
-    except (FileNotFoundError, TypeError, json.JSONDecodeError, ValueError):
+    except (FileNotFoundError, TypeError, json.JSONDecodeError, ValueError) as e:
+        module_logger.error(f'Ошибка: {e}')
         return []
     else:
+        module_logger.info('Успешное преобразование JSON-файла в Python-объект')
         return json_file
