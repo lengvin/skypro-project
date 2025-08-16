7b867fa315855133f4d6d8a6e08f0ecf421884d5
lengvin <zve565ro@gmail.com>
2025-08-16 14:02:20 +0700

    add functions process_bank_search and process_bank_operations in module processing.py

--- src/processing.py
+++ src/processing.py
@@ -1,4 +1,6 @@
 from src import widget
+from collections import Counter
+import re
 
 
 def filter_by_state(list_of_data: list, state: str = "EXECUTED") -> list:
@@ -21,3 +23,28 @@ def sort_by_date(list_of_data: list, _reverse: bool = True) -> list:
     sorted_list = sorted(list_of_data, key=key_to_sort, reverse=_reverse)
 
     return sorted_list
+
+
+def process_bank_search(data: list[dict], search: str) -> list[dict]:
+    """
+    фильтрация данных по строке в описании
+    """
+    result = []
+    for transaction in data:
+        if re.search(search, transaction.get('description'), flags=re.IGNORECASE):
+            result.append(transaction)
+
+    return result
+
+
+def process_bank_operations(data: list[dict], categories: list) -> dict:
+    """
+    подсчет количества транзакций по категориям
+    """
+    descriptions = []
+    unfounded = []
+    for transaction in data:
+        if transaction.get('description') in categories:
+            descriptions.append(transaction.get('description'))
+
+    return Counter(descriptions)
